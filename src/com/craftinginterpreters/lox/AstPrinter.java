627e78e5c220fa1b08247357ead3bf416f2045c4
Daniel Whitford <whitford.dsw@gmail.com>
2021-11-03 20:06:20 +0000

    functions

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -1,44 +1,66 @@
 package com.craftinginterpreters.lox;
 
-class AstPrinter implements Expr.Visitor<String> {
+import com.craftinginterpreters.lox.Expr.Assign;
+import com.craftinginterpreters.lox.Expr.Logical;
+import com.craftinginterpreters.lox.Expr.Variable;
 
-	String print(Expr expr) {
-		return expr.accept(this);
-	}
+abstract class AstPrinter implements Expr.Visitor<String> {
 
-	@Override
-	public String visitBinaryExpr(Expr.Binary expr) {
-		return parenthesize(expr.operator.lexeme, expr.left, expr.right);
-	}
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
 
-	@Override
-	public String visitGroupingExpr(Expr.Grouping expr) {
-		return parenthesize("group", expr.expression);
-	}
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
 
-	@Override
-	public String visitLiteralExpr(Expr.Literal expr) {
-		if (expr.value == null)
-			return "nil";
-		return expr.value.toString();
-	}
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
 
-	@Override
-	public String visitUnaryExpr(Expr.Unary expr) {
-		return parenthesize(expr.operator.lexeme, expr.right);
-	}
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
 
-	private String parenthesize(String name, Expr... exprs) {
-		StringBuilder builder = new StringBuilder();
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
 
-		builder.append("(").append(name);
-		for (Expr expr : exprs) {
-			builder.append(" ");
-			builder.append(expr.accept(this));
-		}
-		builder.append(")");
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
 
-		return builder.toString();
-	}
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    @Override
+    public String visitVariableExpr(Variable expr) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visitAssignExpr(Assign expr) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String visitLogicalExpr(Logical expr) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -6,83 +6,97 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
-	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: generate_ast <output directory>");
-			System.exit(64);
-		}
-		String outputDir = args[0];
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
 
-		defineAst(outputDir, "Expr", Arrays.asList("Binary   : Expr left, Token operator, Expr right",
-				"Grouping : Expr expression", "Literal  : Object value", "Unary    : Token operator, Expr right"));
-	}
+        defineAst(outputDir, "Expr",
+                Arrays.asList("Assign   : Token name, Expr value", "Binary   : Expr left, Token operator, Expr right",
+                        "Call     : Expr callee, Token paren, List<Expr> arguments",
+                        "Grouping : Expr expression", "Literal  : Object value",
+                        "Logical  : Expr left, Token operator, Expr right", "Unary    : Token operator, Expr right",
+                        "Variable : Token name"
 
-	private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
-		String path = outputDir + "/" + baseName + ".java";
-		PrintWriter writer = new PrintWriter(path, "UTF-8");
+                ));
 
-		writer.println("package com.craftinginterpreters.lox;");
-		writer.println();
-		writer.println("import java.util.List;");
-		writer.println();
-		writer.println("abstract class " + baseName + " {");
+        defineAst(outputDir, "Stmt",
+                Arrays.asList("Block      : List<Stmt> statements",
+                        "If         : Expr condition, Stmt thenBranch," + " Stmt elseBranch",
+                        "Function   : Token name, List<Token> params," +
+                                " List<Stmt> body",
+                        "Expression : Expr expression", "Print      : Expr expression",
+                        "Var        : Token name, Expr initializer", "While      : Expr condition, Stmt body"));
+    }
 
-		defineVisitor(writer, baseName, types);
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-		// The AST classes.
-		for (String type : types) {
-			String className = type.split(":")[0].trim();
-			String fields = type.split(":")[1].trim();
-			defineType(writer, baseName, className, fields);
-		}
+        writer.println("package com.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
 
-		// The base accept() method.
-		writer.println();
-		writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+        defineVisitor(writer, baseName, types);
 
-		writer.println("}");
-		writer.close();
-	}
+        // The AST classes.
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
 
-	private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
-		writer.println("  interface Visitor<R> {");
+        // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
-		for (String type : types) {
-			String typeName = type.split(":")[0].trim();
-			writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
-		}
+        writer.println("}");
+        writer.close();
+    }
 
-		writer.println("  }");
-	}
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
 
-	private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
-		writer.println("  static class " + className + " extends " + baseName + " {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
 
-		// Constructor.
-		writer.println("    " + className + "(" + fieldList + ") {");
+        writer.println("  }");
+    }
 
-		// Store parameters in fields.
-		String[] fields = fieldList.split(", ");
-		for (String field : fields) {
-			String name = field.split(" ")[1];
-			writer.println("      this." + name + " = " + name + ";");
-		}
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + " {");
 
-		writer.println("    }");
+        // Constructor.
+        writer.println("    " + className + "(" + fieldList + ") {");
 
-		// Visitor pattern.
-		writer.println();
-		writer.println("    @Override");
-		writer.println("    <R> R accept(Visitor<R> visitor) {");
-		writer.println("      return visitor.visit" + className + baseName + "(this);");
-		writer.println("    }");
+        // Store parameters in fields.
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
 
-		// Fields.
-		writer.println();
-		for (String field : fields) {
-			writer.println("    final " + field + ";");
-		}
+        writer.println("    }");
 
-		writer.println("  }");
-	}
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" + className + baseName + "(this);");
+        writer.println("    }");
+
+        // Fields.
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+    }
 }
